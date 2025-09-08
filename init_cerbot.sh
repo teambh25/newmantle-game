2cd3df78578dcc2cef5ed326e3ca26c54eb1c062
paststar <ladycar96@gmail.com>
2025-09-08 20:18:45 +0900

    add: nginx and cerbot init

--- init_cerbot.sh
+++ init_cerbot.sh
@@ -1,5 +1,11 @@
-mkdir -p nginx
+#!/bin/sh
 mkdir -p certbot/conf
 mkdir -p certbot/www
 
-# Todo : 환경 변수로 conf 파일 만드는 거 추가
+# 환경 변수로 conf 파일 만들기 (한번 실행해봐야됨...)
+export $(xargs < .env) # 전체 환경 변수를 등록하니 조심해서 쓸것..
+envsubst <./nginx/newmantle.conf.template > ./nginx/newmantle.conf
+
+# 환경 변수로 실핼 할 수 있게 바꾸면 좋을듯...
+sudo docker-compose run --rm certbot certonly --webroot --webroot-path=/var/www/certbot -d {server_url} --email {email}
+# sudo docker-compose run --rm certbot renew # --dry-run

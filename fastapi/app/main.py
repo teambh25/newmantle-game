7cf6247b5f93031a00a04b35bd864c877e16ec6c
paststar <ladycar96@gmail.com>
2025-09-09 23:02:12 +0900

    add: authorization docs

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.cores.event import lifespan
@@ -7,7 +7,26 @@ from app.features.admin.routers import admin_router
 import app.exceptions as exceptions
 from app.features.game.routers import game_router
 
-app = FastAPI(lifespan=lifespan)
+from app.cores.auth import authenticate_admin
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
+
+app = FastAPI(
+    lifespan=lifespan,
+    docs_url=None,
+    redoc_url=None,
+    openapi_url="/openapi.json",
+)
+
+@app.get("/docs", include_in_schema=False)
+async def get_documentation(_: bool = Depends(authenticate_admin)):
+    return get_swagger_ui_html(openapi_url=app.openapi_url, title="Docs")
+
+# 4. OpenAPI 스키마 자체도 보호
+@app.get(app.openapi_url, include_in_schema=False)
+async def get_open_api_endpoint(_: bool = Depends(authenticate_admin)):
+    return get_openapi(title="newmantle API", version="2.0.0", routes=app.routes)
+
 app.include_router(admin_router)
 app.include_router(game_router)
 app.add_middleware(
